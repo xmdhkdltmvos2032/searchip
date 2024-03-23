150bfeb7d6756830b55c88b49e9b7551fe738359
twice <jbh2032@naver.com>
2024-03-23 14:50:50 +0900

    hihi

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const app = express();
 const ejs = require("ejs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
 
     let ip = req.connection.remoteAddress;
 
+    if (ip && ip.includes('::ffff:')) {
+        ip = ip.replace('::ffff:', '');
+    }
 
-    res.render("index",{data:ip})
+    res.render("index", { data: ip });
 });
 
 
-app.listen(3000,()=>console.log("server start"));
+app.listen(3000, () => console.log("server start"));
