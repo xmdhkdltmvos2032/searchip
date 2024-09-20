c91b3a170a2889b87bd413f26733c7e56a262fba
xmdhkdltmvos2032 <85145889+xmdhkdltmvos2032@users.noreply.github.com>
2024-09-20 18:17:56 +0900

    dasdasdasdadsdasd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send("예지야겁나사랑한다!!!")
+    res.send("예지야겁나사랑한다!!!!!!!!!!")
 });
 
 
